19703b9f6ba85f4b82615b130961e4268a3aec10
Danilo Roque <dsr.proj3ct@gmail.com>
2023-03-20 16:51:17 -0300

    implementado env

--- services/user.js
+++ services/user.js
@@ -1,4 +1,8 @@
+import jwt from "jsonwebtoken"
+
 let users = []
+
+const SECRET = process.env.JWT_SECRET
 
 export function cadastro(body) {
   const user = users.find(({email}) => email === body.email)
@@ -17,3 +21,22 @@ export function login(body){
 
   return user
 }
+
+
+// export function cadastro(body) {
+//   const user = users.find(({email}) => email === body.email)
+//   if (user) throw new Error('Usuário já cadastrado');
+
+//   users.push(body)
+//   return body
+// }
+
+// export function login(body){
+//   const user = users.find(({email}) => email === body.email)
+//   if (!user) 
+//         throw new Error ('Usuario não encontrado')
+//   if(user.password !== body.password)
+//         throw new Error('Senha Incorreta')
+
+//   return user
+// }
